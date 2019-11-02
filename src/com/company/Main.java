2591f9ec78147c56d1d1aeebd11987d98a578237
Ибрахимов Айрат <Ibrahimov.AM@net.sch2.ru>
2019-11-02 09:22:41 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,22 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner a = new Scanner (System.in);
+        String str1 = a.nextLine();
+        Character k;
+        String m ;
+        String str0 = "";
+        for (int i =0 ; i<str1.length(); i++){
+            k = (str1.charAt(i));
+            m = k.toString();
+            if (m.equals("и")){
+                str0 = str0+m;
+            }
+        }
+        System.out.println(str0);
+    }
+}
